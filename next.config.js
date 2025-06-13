d0a392a0d1134ac1b2b1656dc9e443ab48c04dd3
npybird <firstbird14120@gmail.com>
2025-06-13 12:07:20 +0700

    next.config.js fixed

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    modularizeImports: true,
+  },
+  webpack: (config) => {
+    config.optimization.splitChunks = {
+      chunks: 'all',
+      minSize: 20000, // 20KB
+      maxSize: 24000000, // 24MB (ใกล้ลิมิต)
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
