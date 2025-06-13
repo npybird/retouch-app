e228eec442393b67061d445f336729c99d37b6fb
npybird <firstbird14120@gmail.com>
2025-06-13 14:37:00 +0700

    next.config.js fixed 3

--- next.config.js
+++ next.config.js
@@ -1,20 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig = withBundleAnalyzer({
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    serverComponentsExternalPackages: ["@supabase/supabase-js"],
-  },
-  webpack: (config) => {
-    config.optimization.splitChunks = {
-      chunks: 'all',
-      minSize: 20000,
-      maxSize: 24000000,
-    };
-    config.optimization.removeEmptyChunks = true;
-    return config;
-  },
-};
+});
 
 module.exports = nextConfig;
